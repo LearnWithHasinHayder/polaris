5a2d6265595a57045c5f543a861e0b9bf4571eb7
Hasin Hayder <countdraculla@gmail.com>
2024-01-25 21:47:53 +0600

    React with Polaris

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 })->middleware(['verify.shopify'])->name('home');
 
 
